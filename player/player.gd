ea377391a6760de245519aa05c96b26df4130c10
manspider-thatdev <peterson.parker.james@gmail.com>
2024-02-19 12:12:06 -0600

    Changed shadow effect

--- player/player.gd
+++ player/player.gd
@@ -4,12 +4,16 @@ extends Sprite2D
 
 @onready var cast_dict = {Vector2.DOWN: $DownCast, Vector2.RIGHT: $RightCast, 
 						  Vector2.LEFT: $LeftCast, Vector2.UP: $UpCast}
+@onready var player_glow = $PlayerGlow
 
 
 var tween: Tween
 
 
 func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		player_glow.visible = !player_glow.visible
+	
 	var move_vector: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if move_vector.x != 0:
 		move_vector.y = 0
