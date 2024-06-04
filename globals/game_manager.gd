a5a716687a6d7a876b63cdf67dd0101f419152c0
manspider-thatdev <peterson.parker.james@gmail.com>
2024-06-04 17:50:44 -0500

    Level completion added
    just go to next level

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -4,4 +4,24 @@ extends Node
 signal game_turn(turn_time)
 
 
+@onready var levels: Array[String] = [
+	"res://levels/level_1.tscn", 
+	"res://levels/level_2.tscn", 
+	"res://levels/level_3.tscn", 
+	"res://levels/level_4.tscn",
+	]
+
 var turn_time: float = 0.5
+var level_id: int = 0
+
+
+func load_next_level(current_level: Object):
+	level_id += 1
+	
+	if level_id >= levels.size():
+		level_id = 0
+	
+	var next_level = load(levels[level_id]).instantiate()
+	
+	get_tree().root.add_child(next_level)
+	current_level.queue_free()
--- objects/ladder.gd
+++ objects/ladder.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_area_entered(_area):
 	await get_tree().process_frame
-	get_tree().reload_current_scene() # Start New Level
+	GameManager.load_next_level(get_parent())
