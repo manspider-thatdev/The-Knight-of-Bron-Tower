7dc58af8771e34cb37305d91f8e82ec13a53c980
manspider-thatdev <peterson.parker.james@gmail.com>
2024-06-14 16:45:38 -0500

    Ordered Levels Appropriately
    Makes sense don't it.

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -5,21 +5,21 @@ signal game_turn(turn_time)
 
 
 @onready var levels: Array[String] = [
-	"res://levels/level_1.tscn", 
-	"res://levels/knight_wing.tscn",
-	"res://levels/knights_move.tscn",
-	"res://levels/knight_knight_knight.tscn",
-	"res://levels/knight_cycles.tscn",
-	"res://levels/level_fire_pit.tscn", 
-	"res://levels/in_the_walls.tscn",
-	"res://levels/level_key_door.tscn",
-	"res://levels/knight_patrol.tscn",
-	"res://levels/level_with_some_fake_walls.tscn",
-	"res://levels/knight_watch.tscn",
-	"res://levels/level__with_fire_quirk.tscn",
-	"res://levels/double_duty.tscn",
-	"res://levels/midknight_snack.tscn",
-	"res://levels/epic_level.tscn",
+	"res://levels/level_1.tscn", 					# 1
+	"res://levels/knight_wing.tscn",				# 2
+	"res://levels/knights_move.tscn",				# 3
+	"res://levels/level_key_door.tscn",				# 4
+	"res://levels/knight_patrol.tscn",				# 5
+	"res://levels/level_with_some_fake_walls.tscn",	# 6
+	"res://levels/level_fire_pit.tscn", 			# 7
+	"res://levels/in_the_walls.tscn",				# 8
+	"res://levels/knight_cycles.tscn",				# 9
+	"res://levels/knight_knight_knight.tscn",		# 10
+	"res://levels/double_duty.tscn",				# 11
+	"res://levels/level__with_fire_quirk.tscn",		# 12
+	"res://levels/knight_watch.tscn",				# 13
+	"res://levels/midknight_snack.tscn",			# 14
+	"res://levels/epic_level.tscn",					# 15
 	]
 
 var turn_time: float = 0.5
@@ -33,17 +33,13 @@ var farthest_level: int = 0
 
 func load_level(load_level_id):
 	level_id = load_level_id
-	var level = load(levels[level_id]).instantiate()
-	get_tree().root.add_child(level)
+	get_tree().change_scene_to_file(levels[level_id])
 
 
-func load_next_level(current_level: Object):
+func load_next_level():
 	level_id += 1
 	
 	if level_id >= levels.size():
 		level_id = 0
 	
-	var next_level = load(levels[level_id]).instantiate()
-	
-	get_tree().root.add_child(next_level)
-	current_level.queue_free()
+	get_tree().change_scene_to_file(levels[level_id])
--- objects/ladder/ladder.gd
+++ objects/ladder/ladder.gd
@@ -4,4 +4,4 @@ extends Area2D
 func _on_area_entered(_area):
 	await get_tree().process_frame
 	AudioManager.win_audio.play()
-	GameManager.load_next_level(get_parent())
+	GameManager.load_next_level()
